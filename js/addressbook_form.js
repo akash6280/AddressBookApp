e5f9b719b637e6274a413613285fd07e3305d45c
Akash Singh <akashsingh6280@gmail.com>
2021-10-20 03:45:45 +0530

    ADDED: event listener for validating name

---
+++ js/addressbook_form.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded',(event) => {
+
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('.name-error');
+    name.addEventListener('input',function(){
+        if(name.value.length == 0){
+            nameError.textContent = "";
+            return;
+        }
+        try{
+            new AddressBookData().name=name.value;
+            nameError.textContent="";
+            
+        } catch(e){
+            nameError.textContent = e;
+        }
+    })
+});
