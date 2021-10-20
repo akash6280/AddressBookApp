5df4e0f4e93bc03f9df6318625228788a870e9a3
Akash Singh <akashsingh6280@gmail.com>
2021-10-20 20:16:50 +0530

    REFACTORED: To populate table from local storage

--- js/home.js
+++ js/home.js
@@ -1,8 +1,17 @@
+let contactDataList;
 window.addEventListener("DOMContentLoaded", (event) => {
+    contactDataList = getContactDataFromStorage();
+    document.querySelector(".address-count").textContent = contactDataList.length;
     createInnerHtml();
-  });
+    localStorage.removeItem('editContact');
+});
+
+const getContactDataFromStorage = () => {
+    return localStorage.getItem('AddressBookList') ?JSON.parse(localStorage.getItem('AddressBookList')) : [];
+}
 
 const createInnerHtml = () => {
+    if(contactDataList.length == 0) return;
     const headerHtml = `<tr>
                         <th>FullName</th>
                         <th>Address</th>
@@ -10,21 +19,25 @@ const createInnerHtml = () => {
                         <th>State</th>
                         <th>Zip code</th>
                         <th>Phone Number</th>
-                        </tr>`;
-    const innerHtml = `${headerHtml}
+                        </tr>`;                    
+    let innerHtml = `${headerHtml}`;
+    for(const contactData of contactDataList) {
+        innerHtml = `${innerHtml}  
                         <tr>
-                        <td>Akash</td>
-                        <td>Cantt</td>
-                        <td>Varanasi</td>
-                        <td>Uttar Pradesh</td>
-                        <td>221002</td>
-                        <td>9878767865</td>
+                        <td>${contactData._name}</td>
+                        <td>${contactData._address}</td>
+                        <td>${contactData._city}</td>
+                        <td>${contactData._state}</td>
+                        <td>${contactData._zipcode}</td>
+                        <td>${contactData._phoneNumber}</td>
                         <td>
-                            <img id="1" onclick="remove(this)" alt="delete"
+                        <td>
+                            <img id="${contactData._id}" onclick="remove(this)" alt="delete"
                             src="../assets/icons/delete-black-18dp.svg" style="padding-right: 5px;">
-                            <img id="1" alt="edit" onclick="update(this)"
+                            <img id="${contactData._id}" alt="edit" onclick="update(this)"
                             src="../assets/icons/create-black-18dp.svg">
                         </td>
                         </tr>`;
+    }
     document.querySelector("#table-display").innerHTML = innerHtml;
 }; 
