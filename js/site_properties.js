566ec0eafb85afd2c1cb0d27018573aa7056a54b
Akash Singh <akashsingh6280@gmail.com>
2021-10-21 01:45:38 +0530

    MODIFIED: local storage set to false

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local_storage: "true",
+    use_local_storage: "false",
     home_page: "../pages/home.html",
     add_contact_data_page: "../pages/addressbook_form.html",
     server_url: "http://localhost:3000/ContactDB/"
